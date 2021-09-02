48de71fdd2869e2fb5b178a9d729db9946da40f1
Jason Bodzy <jasonbodzy@gmail.com>
2021-09-02 19:57:39 -0400

    Add files via upload

---
+++ Max_Function/sketch.js
@@ -0,0 +1,24 @@
+// Maximum Function By Jason Bodzy
+// Returns the largest number out of an array of integers
+
+function setup() {
+  createCanvas(400, 400);
+  test_list = [400, 0.5, 100];
+  print(maximum(test_list));
+}
+
+
+function maximum(list){
+  largest = list[0];
+  for(var i in list){
+    current = list[i];
+    if(current > largest){
+      largest = current;
+    }
+    
+  }
+  
+  return(largest);
+  
+}
+
