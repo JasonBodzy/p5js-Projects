184c03bcf9824103461f260bcdf8b49988ad1339
Jason Bodzy <jasonbodzy@gmail.com>
2021-10-13 13:10:38 -0400

    Update sketch.js

--- Crossy_Roads/sketch.js
+++ Crossy_Roads/sketch.js
@@ -11,6 +11,8 @@ function setup() {
 function draw() {
   background(220);
   
+  //Creates tile system
+  
   grid.push(line(0,50,400,50),line(0,100,400,100),line(0,150,400,150),line(0,200,400,200),line(0,250,400,250),line(0,300,400,300),line(0,350,400,350));
   grid.push(line(50,0,50,400),line(100,0,100,400),line(150,0,150,400),line(200,0,200,400),line(250,0,250,400),line(300,0,300,400),line(350,0,350,400));
   
